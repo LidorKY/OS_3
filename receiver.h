6c3197a54aee2715f31ca6788d5c61aed46fc952
GalBS10 <117944971+GalBS10@users.noreply.github.com>
2023-05-12 23:02:11 +0300

    Add files via upload

--- receiver.h
+++ receiver.h
@@ -3,7 +3,7 @@
 #include "unistd.h"
 #include <stdint.h>
 
-void hash_2(uint8_t *array, size_t array_size);
+void hash_2(uint8_t *array, size_t array_size,int q_flag);
 int ipv4_tcp_receiver(char *IP, char *port, int sock, int q_flag);
 int ipv4_udp_receiver(char *IP, char *port, int sock, int q_flag);
 int ipv6_tcp_receiver(char *IP, char *port, int sock, int q_flag);
